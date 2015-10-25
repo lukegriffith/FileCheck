ce2500fdb2da2efc5d840b74073f7e46befc62d1
Luke M Griffith <lukemgriffith@gmail.com>
2015-10-25 22:31:05 +0000

    FileCheck class based DSC Provider
    Class based DSC provider, based on WMF 5 classes in PowerShell.

---
+++ FileCheck.psm1
@@ -0,0 +1,92 @@
+﻿enum Ensure {
+    Present
+    Absent
+}
+
+[DscResource()]
+Class FileCheck {
+
+    [DscProperty(Key, Mandatory)]
+    [String]$FileName
+
+    [DscProperty(Mandatory)]
+    [string]$path
+
+    [DSCProperty(Mandatory)]
+    [Ensure]$Ensure
+    
+    [DSCProperty()]
+    [String]$SourceFilePath
+
+    [DscProperty(NotConfigurable)]
+    [boolean]$Present
+
+
+    [FileCheck]Get()
+    {
+    
+        $FilePath = Join-Path -Path $this.path -ChildPath $this.FileName    
+        $this.Present = $this.TestFile($FilePath)
+
+        return $this
+    }
+    
+    
+    [void]Set(){
+    
+    }
+    
+    [boolean]Test(){
+
+        $FilePath = Join-Path -Path $this.path -ChildPath $this.FileName    
+        $this.Present = $this.TestFile($FilePath)
+
+        if ($this.Ensure -eq 'Present')
+        {
+            if ($this.Present -eq $true)
+            {
+                return $true
+            } else {
+                return $false
+            } 
+        }
+        else
+        {
+            if ($this.Present -eq $true)
+            {
+                return $false
+            } else {
+                return $true
+            }
+        }        
+    
+    }
+
+    [bool]TestFile($filePath){
+                
+        if (Get-Item $filePath){
+            return $true
+        } else {
+            return $false
+        }
+    }
+}
+
+
+
+
+<# Class Based PSCmdlets? 
+
+Class FileCopy : System.Management.Automation.PSCmdlet 
+{
+
+    [string]$param
+
+    [VOID]Test(){
+
+    $this.param = erer
+
+    }
+
+}
+#>
